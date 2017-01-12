fd036016ace836667598ec895aae86bd85cc5479
qiushiwang1994 <w1994@scc2102-22.ucdavis.edu>
2017-01-12 11:34:35 -0800

    test commit

---
+++ studio1/script.js
@@ -0,0 +1,31 @@
+// p5 file
+
+// open the console window of any browser to see console.log() messages,
+// like print() messages in Processing
+console.log("this is a js comment");
+
+/* void setup(){
+  size(800,250);
+  background(255);
+}*/
+
+function setup() {
+  var myCanvas=createCanvas(800, 250);
+  myCanvas.parent('mySketch');
+  background(255);
+}
+
+
+function draw() {
+  // statements here
+
+   if(mouseIsPressed){
+
+  fill(0);
+}
+else{
+  fill(255);
+}
+ellipse(mouseX, mouseY, 50, 50);
+
+}
